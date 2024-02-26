86b07f3248759eb9259cfc0df61c7b5f0b8dea8a
FELIPE8976 <lufema2003@gmail.com>
2024-02-26 16:52:15 -0500

    Cambio del idioma, arreglo de errores de impresión

--- ventas/forms.py
+++ ventas/forms.py
@@ -3,19 +3,19 @@ from django.core.exceptions import ValidationError
 from .models import Product
 
 class CreateNewProduct(forms.Form):
-   name = forms.CharField(label='Ingrese el nombre del producto', max_length=200)
-   stock = forms.IntegerField(label='Ingrese la cantidad')
+   name = forms.CharField(label='Enter the name of the new product', max_length=200)
+   stock = forms.IntegerField(label='Enter the quantity')
 
    def clean_name(self):
       name = self.cleaned_data.get('name')
       if name:
          if Product.objects.filter(name =name).exists():
-            raise ValidationError('Este producto ya existe')
+            raise ValidationError('This product already exist')
       return name
    
 class DeleteProduct(forms.Form):
-   product = forms.ModelChoiceField(queryset=Product.objects.all(), label='Seleccione el producto a eliminar')
+   product = forms.ModelChoiceField(queryset=Product.objects.all(), label='Select a product to delete',empty_label='None')
 
 class UpdateProduct(forms.Form):
-   product = forms.ModelChoiceField(queryset=Product.objects.all(), label='Seleccione el producto a actualizar')
-   stock = forms.IntegerField(label='Ingrese la nueva cantidad', min_value=0)
+   product = forms.ModelChoiceField(queryset=Product.objects.all(), label='Select a product to update')
+   stock = forms.IntegerField(label='Enter a new quantity', min_value=0)
