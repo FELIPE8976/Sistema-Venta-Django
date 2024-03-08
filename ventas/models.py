3d1188dcf60cc80fcd891be724af853931801d8a
Carlos Camacho <carloscamacho0602@gmail.com>
2024-03-07 19:27:35 -0500

    Se agregaron los campos unit_price y have_discount al modelo de Productos

--- ventas/models.py
+++ ventas/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Product(models.Model):
    name = models.CharField(max_length=200)
    stock = models.IntegerField(default=0)
+   unit_price = models.DecimalField(default = 0.00,max_digits=5, decimal_places=2)
+   have_discount = models.BooleanField(default=False)
+
 
    def __str__(self):
       return self.name
